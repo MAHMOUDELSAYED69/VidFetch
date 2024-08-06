2868e7764640603f5ea96e1fb1082fe8dc0fd752
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-08-06 03:27:48 +0300

    add more assets, create color manager

---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorManager {
+  const ColorManager._();
+
+  static const Color white = Color(0xffFFFFFF);
+  static const Color grey = Color(0xff6C6C6C);
+  static const Color darkGrey = Color(0xff3B3B3B);
+  static const Color lightGrey = Color(0xff9D9D9D);
+  static const Color black = Color(0xff000000);
+  static const Color red = Color(0xffFF0000);
+}
--- lib/utils/constants/images.dart
+++ lib/utils/constants/images.dart
@@ -1,4 +1,6 @@
 abstract class ImageManager {
   const ImageManager._();
   static const String logo = "assets/images/logo.png";
+  static const String youtube = "assets/images/youtube.png";
+  static const String shorts = "assets/images/shorts.png";
 }
