761ef4ecf2c9b4cf40e03069fe91d131425c3fe8
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-08-08 01:52:07 +0300

    handle the screen size on windows

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,33 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:window_manager/window_manager.dart';
 
 import 'app.dart';
 
-void main() {
+Future<void> main() async {
+  if (Platform.isWindows) {
+    WidgetsFlutterBinding.ensureInitialized();
+    await windowManager.ensureInitialized();
+
+    WindowOptions windowOptions = const WindowOptions(
+      size: Size(450, 600),
+      center: true,
+      backgroundColor: Colors.transparent,
+      title: 'Vid Fetch',
+      titleBarStyle: TitleBarStyle.normal,
+      skipTaskbar: false,
+    );
+
+    windowManager.waitUntilReadyToShow(windowOptions, () async {
+      await windowManager.show();
+      await windowManager.focus();
+      await windowManager.setResizable(false);
+      await windowManager.setMinimumSize(const Size(450, 600));
+      await windowManager.setMaximizable(false);
+    });
+  }
+
   runApp(const ProviderScope(child: MyApp()));
 }
